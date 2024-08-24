b9a908a24206e88795bf7c844df0d3a20eeec87a
Aniket Gupta <aniketgupta07122001@gmail.com>
2024-08-24 14:44:14 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Main{
+    
+     public static void main(String[] args){
+        System.out.println("This is a java Program");
+        System.out.println("Output from java main");
+        System.out.println(new Date());
+     }
+}
